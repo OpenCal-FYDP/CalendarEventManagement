ee74fd2fc0518dfa289c58c011d3af7688207c00
Shivansh Vij <shivanshvij@outlook.com>
2022-03-10 15:00:53 -0800

    client

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/OpenCal-FYDP/CalendarEventManagement
 go 1.17
 
 require (
-	github.com/twitchtv/twirp v8.1.1+incompatible // indirect
-	google.golang.org/protobuf v1.27.1 // indirect
+	github.com/rs/cors v1.8.2
+	github.com/twitchtv/twirp v8.1.1+incompatible
+	google.golang.org/protobuf v1.27.1
 )
+
+require github.com/pkg/errors v0.9.1 // indirect
--- main.go
+++ main.go
@@ -1,14 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"github.com/OpenCal-FYDP/CalendarEventManagement/internal/service"
 	"github.com/OpenCal-FYDP/CalendarEventManagement/rpc"
+	"github.com/rs/cors"
 	"log"
+	"math/rand"
 	"net/http"
+	"os"
 )
 
+func randomString(n int) string {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	s := make([]rune, n)
+	for i := range s {
+		s[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(s)
+}
+
 func main() {
-	service := service.New()
-	server := rpc.NewCalendarEventManagementServiceServer(service)
-	log.Fatal(http.ListenAndServe(":8080", server))
+	secret := os.Getenv("SECRET")
+	if secret == "" {
+		secret = randomString(128)
+		fmt.Printf("Randomly Generated Secret: %s\n", secret)
+	}
+	corsWrapper := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:3000"},
+		AllowedMethods: []string{"POST"},
+		AllowedHeaders: []string{"Content-Type"},
+	})
+	svc := service.New()
+	server := rpc.NewCalendarEventManagementServiceServer(svc)
+	//server := rpc.NewPreferenceManagementServiceServer(svc, twirp.WithServerInterceptors(Authorization.NewAuthorizationInterceptor([]byte(secret), authorizeMethods...)))
+	//jwtServer := Authorization.WithJWT(server)
+	handler := corsWrapper.Handler(server)
+	log.Fatal(http.ListenAndServe(":8080", handler))
 }
