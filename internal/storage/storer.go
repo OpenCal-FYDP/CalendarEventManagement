96a73446ede2a263a9b01c9b0c5f4b122d1540e0
Jonathan Sun <jspsun@gmail.com>
2022-03-10 04:57:06 -0500

    AddTableName

--- internal/storage/storer.go
+++ internal/storage/storer.go
@@ -2,8 +2,10 @@ package storage
 
 import (
 	"github.com/OpenCal-FYDP/CalendarEventManagement/rpc"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
 )
 
@@ -22,7 +24,22 @@ func New() *Storage {
 }
 
 func (s *Storage) CreateEvent(req *rpc.CreateEventReq) error {
-	panic("a")
+	av, err := dynamodbattribute.MarshalMap(req)
+
+	if err != nil {
+		return err
+	}
+
+	// Now create put item
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String(userTable),
+	}
+
+	// Now we push it into the database, then return
+	_, err = s.client.PutItem(input)
+
+	return err
 }
 
 func (s *Storage) UpdateEvent(req *rpc.UpdateEventReq) error {
