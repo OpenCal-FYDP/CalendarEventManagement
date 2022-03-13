c9ca67ef2566725512b966b7ec8432b8ae40ed92
Jonathan Sun <Jspsun@gmail.com>
2022-03-13 06:19:06 -0400

    Add twirpscript and cors (#8)
    * Update twirp script files
    
    * Add cors managment
    
    Co-authored-by: Shivansh Vij <shivanshvij@outlook.com>

--- main.go
+++ main.go
@@ -1,16 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"github.com/OpenCal-FYDP/CalendarEventManagement/internal/service"
 	"github.com/OpenCal-FYDP/CalendarEventManagement/rpc"
+	"github.com/rs/cors"
 	"log"
+	"math/rand"
 	"net/http"
+	"os"
 )
 
+func randomString(n int) string {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	s := make([]rune, n)
+	for i := range s {
+		s[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(s)
+}
+
 func main() {
+	secret := os.Getenv("SECRET")
+	if secret == "" {
+		secret = randomString(128)
+		fmt.Printf("Randomly Generated Secret: %s\n", secret)
+	}
+	corsWrapper := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:3000"},
+		AllowedMethods: []string{"POST"},
+		AllowedHeaders: []string{"Content-Type"},
+	})
 	svc := service.New()
 	server := rpc.NewCalendarEventManagementServiceServer(svc)
-	log.Fatal(http.ListenAndServe(":8080", server))
+	handler := corsWrapper.Handler(server)
+
+	log.Fatal(http.ListenAndServe(":8080", handler))
 }
 
 // uncomment and run to manually get new oauth token
